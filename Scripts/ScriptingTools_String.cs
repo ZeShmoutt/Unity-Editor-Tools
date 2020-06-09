85e82a08dc15b134d1b6382dfd8fd6515c92ae68
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-06-10 00:26:13 +0200

    Added String.IsAnyNullOrEmpty

--- Scripts/ScriptingTools_String.cs
+++ Scripts/ScriptingTools_String.cs
@@ -25,6 +25,37 @@
 			}
 
 			#endregion
+
+			#region Is any null or empty
+
+			/// <summary>
+			/// Checks if a list of strings contains at least one that is null or empty.
+			/// </summary>
+			/// <param name="strings">List to check.</param>
+			/// <returns>Returns true if the list has an item that is null or empty.</returns>
+			public static bool IsAnyNullOrEmpty(List<string> strings)
+			{
+				return IsAnyNullOrEmpty(strings.ToArray());
+			}
+
+			/// <summary>
+			/// Checks if an array of strings contains at least one that is null or empty.
+			/// </summary>
+			/// <param name="strings">Array to check.</param>
+			/// <returns>Returns true if the array has an item that is null or empty.</returns>
+			public static bool IsAnyNullOrEmpty(string[] strings)
+			{
+				for (int i = 0; i < strings.Length; i++)
+				{
+					if (string.IsNullOrEmpty(strings[i]))
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+
+			#endregion
 		}
 	}
 }
